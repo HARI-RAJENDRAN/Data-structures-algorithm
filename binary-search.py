b18a2985d6f73319684e90b8b852e0e0e72bf2b0
HARI KRISHNAN <141993320+HARI-RAJENDRAN@users.noreply.github.com>
2025-10-30 23:31:47 +0530

    Add files via upload

---
+++ binary-search.py
@@ -0,0 +1,21 @@
+def binary_search(arr,target):
+    low = 0
+    high = len(arr)-1
+    
+    while low <= high:
+        mid = (low+high)//2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid +1
+        else :
+            high = mid - 1
+            
+    return -1
+    
+arr = [10,20,30,40,50,60,80]
+target = 10
+result = binary_search(arr,target)
+
+print("result:",arr[result])
